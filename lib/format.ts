369f65b02d2009bfa1bfa989b53a6034b2bfc76f
edgarjjhaaa <edgarjjhaaa@gmail.com>
2026-09-08 07:10:32 -0300

    Ajuste fuso horario

--- lib/format.ts
+++ lib/format.ts
@@ -71,6 +71,7 @@ export function formatDateTime(iso: string | null | undefined) {
   return new Intl.DateTimeFormat('pt-BR', {
     dateStyle: 'short',
     timeStyle: 'short',
+    timeZone: 'America/Sao_Paulo',
   }).format(date)
 }
 
